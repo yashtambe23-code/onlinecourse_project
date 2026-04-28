43cb091bda980442b51cd62d011c081286ae648c
yashtambe23-code <yash.tambe23@vit.edu>
2026-04-28 11:17:45 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Instructor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Learner(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
@@ -7,11 +14,24 @@ class Lesson(models.Model):
     title = models.CharField(max_length=100)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
+class Enrollment(models.Model):
+    learner = models.ForeignKey(Learner, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
 class Question(models.Model):
-    text = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(id__in=selected_ids, is_correct=True).count()
+        return all_answers == selected_correct
 
 class Choice(models.Model):
-    text = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
 
 class Submission(models.Model):
-    student = models.CharField(max_length=100)
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
